1ce5614f3d255251f1cf5f227d35dc57a4045ed8
wendygrande1999 <elizabethalvarezgra17@gmail.com>
2024-09-10 22:25:46 -0600

    Se agrega un h1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,8 @@
             <tr>
                 <td>
                 <div class="card" style="width: 18rem;">
+
+                <h1>Esta es una prueba</h1>
                    <img  src="{{ asset('assets/playa.png')}}" class="card-img-top" alt="150px">
 
                     <div class="card-body">
