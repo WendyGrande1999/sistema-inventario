880fa876b72e0991b60cd909572665c2c608d7ca
LuzHENRIQUEZ <luzhenriquez2018@gmail.com>
2024-10-03 17:56:23 -0600

    Canbios en el navbar de recuperar contraseña

--- resources/views/partials/guest/navbar.blade.php
+++ resources/views/partials/guest/navbar.blade.php
@@ -1,10 +1,10 @@
 <!-- resources/views/partials/guest/navbar.blade.php -->
 
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm menu-horizontal">
     <div class="container">
-       
+
         <a class="navbar-brand" href="#">{{ __('Hojas Eco Villa') }}</a>
-        
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarGuest" aria-controls="navbarGuest" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -16,8 +16,9 @@
                         <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
                     </li>
                 @endif
-               
+
             </ul>
         </div>
     </div>
 </nav>
+
